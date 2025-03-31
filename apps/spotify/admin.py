5d53e8d6be76064ae08d4ff28a908ff50f802b53
Your Name <you@example.com>
2025-03-31 17:57:26 +0300

    first commit

--- apps/spotify/admin.py
+++ apps/spotify/admin.py
@@ -10,7 +10,15 @@ from apps.spotify.models import *
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('id', 'name', 'surname', 'album_count')
-    list_display_links = ('name', 'album_count')
+    list_display_links = ('name','surname', 'album_count')
     list_filter = ('name', 'surname', 'album_count')
     search_fields = ('name', 'surname')
     ordering = ('id', 'name', 'surname')
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('id', 'author', 'name')
+    list_display_links = ('id', 'author', 'name')
+    list_filter = ('id', 'author', 'name')
+    search_fields = ('id', 'author', 'name')
+    ordering = ('id', 'author', 'name')
