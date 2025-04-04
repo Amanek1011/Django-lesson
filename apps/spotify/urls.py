da0b40f956e8dda35aa1370c65bc5b515daa0b13
Amanek1011 <kkat51866@gmail.com>
2025-04-04 22:58:01 +0600

    добавление song и author list

--- apps/spotify/urls.py
+++ apps/spotify/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from .views import hello
+from . import views
 
 urlpatterns = [
     path('', hello, name='hello'),
+    path('author_list', views.authors_list, name = 'author_list'),
+    path('author_list_2', views.authors_list, name = 'author_list_2'),
+    path('song_list', views.songs_list, name = 'song_list'),
+    path('song_list_2',views.songs_list, name = 'song_list_2')
 ]
--- apps/spotify/views.py
+++ apps/spotify/views.py
@@ -1,5 +1,28 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import ListView
+
+from apps.spotify.models import Author, Song
 
 
 def hello(request):
     return HttpResponse('Hello, world!')
+
+
+def authors_list(request):
+    authors = Author.objects.all()
+    return render(request, 'spotify/authors_list.html',{'authors': authors})
+
+class AuthorsListView(ListView):
+    model = Author
+    template_name = 'spotify/authors_list_2.html'
+    context_object_name = 'authors'
+
+def songs_list(request):
+    songs = Song.objects.all()
+    return render(request,'spotify/songs_list.html',{'songs': songs})
+
+class SongsListView(ListView):
+    model = Song
+    template_name = 'spotify/songs_list_2.html'
+    context_object_name = 'songs'
